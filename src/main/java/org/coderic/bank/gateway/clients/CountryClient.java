f3aca5214995bb5bf95676d0e0df133c756aa8d0
Neftalí Yagua <neftaliyagua@fedoraproject.org>
2024-03-08 23:19:56 -0500

    Update CountryClient.java

--- src/main/java/org/coderic/bank/gateway/clients/CountryClient.java
+++ src/main/java/org/coderic/bank/gateway/clients/CountryClient.java
@@ -19,7 +19,7 @@ public class CountryClient extends WebServiceGatewaySupport {
         log.info("Requesting location for " + country);
 
         GetCountryResponse response = (GetCountryResponse) getWebServiceTemplate()
-                .marshalSendAndReceive("https://staging.coderic.org:8443/endpoint/ws/countries", request,
+                .marshalSendAndReceive("https://staging.coderic.org:8443/bank/endpoint/ws/countries", request,
                         new SoapActionCallback(
                                 "http://coderic.org/bank/ws/GetCountryRequest"));
 
